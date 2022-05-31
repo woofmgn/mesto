e6dc355f70a52a5d75a3435a560fc0952f8299c5
Denis Vakulenko <woof88@yandex.ru>
2022-05-31 16:46:21 +0500

    feat: added template and cards array

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const openPopup = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__close-window');
@@ -7,6 +34,30 @@ const userJob = document.querySelector('.profile__subtitle');
 const nameInput = formElement.querySelector('.popup__form-item_type_name');
 const jobInput = formElement.querySelector('.popup__form-item_type_job');
 const saveUserInput = formElement.querySelector('.popup__form-button');
+
+const cardListElement = document.querySelector('.element');
+const cardTemplateElement = document.querySelector('.card-template');
+
+const createCard = item => {
+  const card = cardTemplateElement.content.querySelector('.element__item').cloneNode(true);
+  const cardImage = card.querySelector('.element__image');
+  const cardTitle = card.querySelector('.element__title');
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
+  return card;
+};
+
+const addCard = item => {
+  const card = createCard(item);
+
+  cardListElement.prepend(card);
+};
+
+initialCards.forEach(addCard);
+
+
+
 
 function popupOpen() {
   popup.classList.add('popup_opened');
