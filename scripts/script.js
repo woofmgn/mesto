aa7da76364fe0e2fc040784e41e76a9efc755d3c
Denis Vakulenko <woof88@yandex.ru>
2022-06-02 16:40:09 +0500

    fix: comments of the review

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,7 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const openPopupProfile = document.querySelector('.profile__edit-button');
 const popupProfile = document.querySelector('.popup_type_profile');
 const closePopupProfile = document.querySelector('.popup__close-window');
-const formElementProfile = document.getElementsByName('user-info')[0];
+const formElementProfile = popupProfile.querySelector('.popup__form_type_profile');
 const userName = document.querySelector('.profile__title');
 const userJob = document.querySelector('.profile__subtitle');
 const nameInput = formElementProfile.querySelector('.popup__form-item_type_name');
@@ -39,14 +12,13 @@ const openPopupAddCards = document.querySelector('.profile__add-button');
 const closePopupAddCards = popupAddCards.querySelector('.popup__close-window');
 const cardListElement = document.querySelector('.element');
 const cardTemplateElement = document.querySelector('.card-template');
-const formElementNewPlace = document.getElementsByName('new-place')[0];
+const formElementNewPlace = popupAddCards.querySelector('.popup__form_type_cards');
 const inputPlaceNameNewPlace = formElementNewPlace.querySelector('.popup__form-item_type_place');
 const inputLinkNewPlace = formElementNewPlace.querySelector('.popup__form-item_type_link');
 const popupImage = document.querySelector('.popup_type_image');
 const popupImageFull = document.querySelector('.popup__image-item');
 const popupImageTitle = document.querySelector('.popup__title-image');
 const popupImageClose = document.querySelector('.popup__close-window_type_image');
-
 
 // создаем карточку, прослушиваем лайки/анлайки, прослушиваем удаление карточек,
 // прослушиваем и открывает попап картинки
@@ -99,13 +71,12 @@ function closePopup(item) {
   item.classList.remove('popup_opened');
 };
 
-function formSubmitHandler (evt) {
+function handlerEditProfileFormSubmit (evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userJob.textContent = jobInput.value;
   closePopup(popupProfile);
 };
-
 
 //добавление новой карточки через кнопку
 const handleAddCardsSubmit = evt => {
@@ -118,7 +89,8 @@ const handleAddCardsSubmit = evt => {
 
   const inputValue = createCard(itemInput);
   cardListElement.prepend(inputValue);
-  popupAddCards.classList.remove('popup_opened');
+
+  closePopup(popupAddCards);
   formElementNewPlace.reset();
 };
 
@@ -132,7 +104,7 @@ openPopupProfile.addEventListener('click', () => {
 closePopupProfile.addEventListener('click', () => {
   closePopup(popupProfile);
 });
-formElementProfile.addEventListener('submit', formSubmitHandler);
+formElementProfile.addEventListener('submit', handlerEditProfileFormSubmit);
 
 openPopupAddCards.addEventListener('click', () => {
   openPopup(popupAddCards);
@@ -146,7 +118,6 @@ closePopupAddCards.addEventListener('click', () => {
 popupImageClose.addEventListener('click', () => {
   closePopup(popupImage);
 });
-
 
 formElementNewPlace.addEventListener('submit', handleAddCardsSubmit);
 
